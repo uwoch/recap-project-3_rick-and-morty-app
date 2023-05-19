8a9714caa180c5b28ec1cd026fdfeadd858c7efc
Alexander Bick <AlexanderBick@me.com>
2023-05-19 12:33:02 +0200

    Stand Freitag,  12.30h

--- index.js
+++ index.js
@@ -14,3 +14,59 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+
+
+async function fetchCharacters() {
+  // const url = `https://rickandmortyapi.com/api/character?page=${page}`;
+  const url = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
+
+  //cardContainer.innerHTML = "";
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      data.results.forEach((element) => {
+        createCharacterCard(element);
+      });
+
+      return;
+    } else {
+      console.error("Ooops");
+    }
+  } catch (error) {
+    console.error("ERROR occurred" + error.message);
+  }
+}
+
+fetchCharacters();
+
+/*nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+    pagination.textContent = `${page} / ${maxPage}`;
+    return;
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+    pagination.textContent = `${page} / ${maxPage}`;
+    return;
+  }
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(searchBar);
+  const data = Object.fromEntries(formData);
+  console.log(data.query);
+  searchQuery = data.query;
+  page = 1;
+  pagination.textContent = `${page} / ${maxPage}`;
+  fetchCharacters();
+});*/
